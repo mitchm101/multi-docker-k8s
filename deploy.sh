21d75c7634bc6790db824f7a5abb6a23feb26587
mmyers <mitch@mmyers.org>
2018-12-15 16:32:13 -0800

    Finished deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mitchm101/multi-client:latest -t mitchm101/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mitchm101/multi-server:latest -t mitchm101/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mitchm101/multi-worker:latest -t mitchm101/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mitchm101/multi-client:latest
+docker push mitchm101/multi-server:latest
+docker push mitchm101/multi-worker:latest
+
+docker push mitchm101/multi-client:$SHA
+docker push mitchm101/multi-server:$SHA
+docker push mitchm101/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server-mitchm101/multi-server:$SHA
+kubectl set image deployments/client-deployment client-mitchm101/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker-mitchm101/multi-worker:$SHA
